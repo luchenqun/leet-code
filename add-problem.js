2cb0ce843d43317c9fed0d6721b8a87687049b73
luchenqun <luchenqun@juzix.io>
2018-03-23 18:13:46 +0800

    add problem list

--- add-problem.js
+++ add-problem.js
@@ -2,6 +2,7 @@ let cheerio = require('cheerio');
 let fs = require('fs-extra');
 let promisify = require('util').promisify;
 let fetchUrl = require("fetch").fetchUrl;
+let proList = require("./problem-list.json");
 
 fetchUrl[promisify.custom] = function (url) {
     return new Promise((resolve, reject) => {
@@ -11,16 +12,34 @@ fetchUrl[promisify.custom] = function (url) {
     })
 };
 
-// fs.outputFile[promisify.custom] = function (path, data) {
-//     return new Promise((resolve, reject) => {
-//         fs.outputFile
-//     })
-// }
-
 const fetchUrlAsync = promisify(fetchUrl);
 const outputJsonAsync = promisify(fs.outputJson);
 const readFileAsync = promisify(fs.readFile);
 
+let index = process.argv.length < 3 ? 0 : process.argv[2];
+let type = process.argv.length < 4 ? "js" : process.argv[3];
 
-let type = process.argv.length < 3 ? "js" : process.argv[2];
+(async function (index, type) {
+    try {
+        console.log('begin......', index, type);
+        let pro = proList[index];
+        if (pro || 1) {
+            //let url = "https://leetcode-cn.com/" + pro.href + "/description/";
+            let body = await fetchUrlAsync("https://leetcode-cn.com/problems/two-sum/description/");
+            console.log(body);
+            let $ = cheerio.load(body);
+            console.log($(".question-description").html());
+            // await fs.outputFile('./tmp/blog.html', $blog.html())
+            // await outputJsonAsync('./tmp/mdn.json', json);
+        } else {
+            console.log('problem is not exit...');
+        }
+
+        console.log('end......');
+    } catch (error) {
+        console.log('error:', error);
+    }
+}(index, type));
+
+
 
