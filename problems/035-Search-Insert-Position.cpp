7223ef79be6efe328796a216fc0a1625064f110f
luchenqun <luchenqun@juzix.io>
2018-03-30 08:27:36 +0800

    035-Search-Insert-Position.cpp

--- problems/035-Search-Insert-Position.cpp
+++ problems/035-Search-Insert-Position.cpp
@@ -1,50 +1,53 @@
-/**
-Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
-
-You may assume no duplicates in the array.
-
-Here are few examples.
-[1,3,5,6], 5 → 2
-[1,3,5,6], 2 → 1
-[1,3,5,6], 7 → 4
-[1,3,5,6], 0 → 0
-
-*/
+/*
+ * [35] Search Insert Position
+ *
+ * https://leetcode-cn.com/problems/search-insert-position/description/
+ *
+ * algorithms
+ * Easy (40.06%)
+ * Total Accepted:    448
+ * Total Submissions: 1.1K
+ * Testcase Example:  '[1,3,5,6]\n5'
+ *
+ * 给定一个排序数组和一个目标值，如果在数组中找到目标值则返回索引。如果没有，返回到它将会被按顺序插入的位置。
+ * 你可以假设在数组中无重复元素。
+ * 案例 1:
+ * 输入: [1,3,5,6], 5
+ * 输出: 2
+ * 案例 2:
+ * 输入: [1,3,5,6], 2
+ * 输出: 1
+ * 案例 3:
+ * 输入: [1,3,5,6], 7
+ * 输出: 4
+ * 案例 4:
+ * 输入: [1,3,5,6], 0
+ * 输出: 0
+ */
 
 #include <iostream>
-#include <vector>
 #include <algorithm>
-
+#include <vector>
 using namespace std;
 
 class Solution
 {
-public:
-  int searchInsert(vector<int> &nums, int target)
-  {
-    return lower_bound(nums.begin(), nums.end(), target) - nums.begin();
-  }
+  public:
+    int searchInsert(vector<int> &nums, int target)
+    {
+        auto iter = lower_bound(nums.begin(), nums.end(), target);
+        return iter - nums.begin();
+    }
 };
 
 int main()
 {
-  vector<int> nums1{1, 3, 5, 6};
-  int target1 = 5;
-  vector<int> nums2{1, 3, 5, 6};
-  int target2 = 2;
-  vector<int> nums3{1, 3, 5, 6};
-  int target3 = 7;
-  vector<int> nums4{1, 3, 5, 6};
-  int target4 = 0;
-  vector<int> nums5{1, 3, 5, 7, 9};
-  int target5 = 5;
+    Solution s;
+    vector<int> nums{1, 3, 5, 6};
+    cout << s.searchInsert(nums, 5) << endl;
+    cout << s.searchInsert(nums, 2) << endl;
+    cout << s.searchInsert(nums, 7) << endl;
+    cout << s.searchInsert(nums, 0) << endl;
 
-  Solution s;
-  cout << s.searchInsert(nums1, target1) << endl;
-  cout << s.searchInsert(nums2, target2) << endl;
-  cout << s.searchInsert(nums3, target3) << endl;
-  cout << s.searchInsert(nums4, target4) << endl;
-  cout << s.searchInsert(nums5, target5) << endl;
-
-  return 0;
+    return 0;
 }
