d71a592421f37249f151e469ffb8f149997d87e0
luchenqun <luchenqun@juzix.io>
2018-05-29 15:25:25 +0800

    ${m}

--- problems/111-minimum-depth-of-binary-tree.cpp
+++ problems/111-minimum-depth-of-binary-tree.cpp
@@ -24,22 +24,32 @@
  */
 
 #include <iostream>
-#include <string>
+#include <algorithm>
+
 using namespace std;
 
 /**
  * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
  */
-class Solution {
-public:
-    int minDepth(TreeNode* root) {
-        
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution
+{
+  public:
+    int minDepth(TreeNode *root)
+    {
+        if (root == NULL)
+            return 0;
+        int left = minDepth(root->left);
+        int right = minDepth(root->right);
+        return (left == 0 || right == 0) ? (left + right + 1) : (min(left, right) + 1);
     }
 };
 
@@ -49,4 +59,3 @@ int main()
 
     return 0;
 }
-        
