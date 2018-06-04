b48219e8def47e73a831f9121e8564d96d0aab54
luchenqun <luchenqun@juzix.io>
2018-06-04 15:16:40 +0800

    add 617-merge-two-binary-trees

--- problems/617-merge-two-binary-trees.cpp
+++ problems/617-merge-two-binary-trees.cpp
@@ -37,17 +37,44 @@ using namespace std;
 
 /**
  * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
  */
-class Solution {
-public:
-    TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) {
-        
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+class Solution
+{
+  public:
+    TreeNode *mergeTrees(TreeNode *t1, TreeNode *t2)
+    {
+        TreeNode *t = NULL;
+        merge(t, t1, t2);
+        return t;
+    }
+
+    void merge(TreeNode *&t, TreeNode *t1, TreeNode *t2)
+    {
+        if (t1 == NULL && t2 == NULL)
+            return;
+        t = t1 ? t1 : t2;
+        t->val = (t1 ? t1->val : 0) + (t2 ? t2->val : 0);
+        merge(t->left, t1 ? t1->left : NULL, t2 ? t2->left : NULL);
+        merge(t->right, t1 ? t1->right : NULL, t2 ? t2->right : NULL);
+    }
+
+    TreeNode *mergeTrees(TreeNode *t1, TreeNode *t2)
+    {
+        if (t1 == NULL)
+            return t2;
+        if (t2 == NULL)
+            return t1;
+        t1->val += t2->val;
+        t1->left = mergeTrees(t1->left, t2->left);
+        t1->right = mergeTrees(t1->right, t2->right);
+        return t1;
     }
 };
 
@@ -57,4 +84,3 @@ int main()
 
     return 0;
 }
-        
